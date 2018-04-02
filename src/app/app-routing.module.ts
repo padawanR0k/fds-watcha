94f1d7c147a5eae8b03ea80cf98ce50179a0e748
hckang <hckang80@gmail.com>
2018-04-02 12:29:14 +0900

    인트로 페이지 추가 및 라우터 설정

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import {
+  HomeComponent,
+  IntroComponent
+} from './pages';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'intro', component: IntroComponent },
+  // { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/pages/index.ts
@@ -0,0 +1,3 @@
+export * from '../home/home.component';
+export * from '../intro/intro.component';
+// export * from './not-found.component';
