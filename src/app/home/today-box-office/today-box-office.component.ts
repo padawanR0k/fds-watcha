7ab42dcacc270449402d9c1cb41aa8f1eb7f3ee8
padawanr0k <padawanr0k@gmail.com>
2018-04-03 16:05:45 +0900

    메인페이지 박스오피스 영화리스트 및 별점평가기능

--- src/app/home/today-box-office/today-box-office.component.ts
+++ src/app/home/today-box-office/today-box-office.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'today-box-office',
@@ -6,10 +6,109 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./today-box-office.component.scss']
 })
 export class TodayBoxOfficeComponent implements OnInit {
+  constructor() {}
+  boxOffice = [
+    {
+      movieNm: '레디플레이어 원',
+      rate: 0,
+      posterUrl:
+        'https://dhgywazgeek0d.cloudfront.net/watcha/image/upload/c_fill,h_700,q_80,w_490/v1521617852/guetszfm472lbu0ypxp2.jpg'
+    },
+    {
+      movieNm: '레디플레이어 원',
+      rate: 0,
+      posterUrl:
+        'https://dhgywazgeek0d.cloudfront.net/watcha/image/upload/c_fill,h_400,q_80,w_280/v1519955226/vvnr4dqs6rxnxbc4mgvq.jpg'
+    },
+    {
+      movieNm: '레디플레이어 원',
+      rate: 0,
+      posterUrl:
+        'https://dhgywazgeek0d.cloudfront.net/watcha/image/upload/c_fill,h_400,q_80,w_280/v1519955226/vvnr4dqs6rxnxbc4mgvq.jpg'
+    },
+    {
+      movieNm: '레디플레이어 원',
+      rate: 0,
+      posterUrl:
+        'https://dhgywazgeek0d.cloudfront.net/watcha/image/upload/c_fill,h_400,q_80,w_280/v1519955226/vvnr4dqs6rxnxbc4mgvq.jpg'
+    },
+    {
+      movieNm: '레디플레이어 원',
+      rate: 0,
+      posterUrl:
+        'https://dhgywazgeek0d.cloudfront.net/watcha/image/upload/c_fill,h_400,q_80,w_280/v1519955226/vvnr4dqs6rxnxbc4mgvq.jpg'
+    },
+    {
+      movieNm: '레디플레이어 원',
+      rate: 0,
+      posterUrl:
+        'https://dhgywazgeek0d.cloudfront.net/watcha/image/upload/c_fill,h_400,q_80,w_280/v1519955226/vvnr4dqs6rxnxbc4mgvq.jpg'
+    },
+    {
+      movieNm: '레디플레이어 원',
+      rate: 0,
+      posterUrl:
+        'https://dhgywazgeek0d.cloudfront.net/watcha/image/upload/c_fill,h_400,q_80,w_280/v1519955226/vvnr4dqs6rxnxbc4mgvq.jpg'
+    },
+    {
+      movieNm: '레디플레이어 원',
+      rate: 0,
+      posterUrl:
+        'https://dhgywazgeek0d.cloudfront.net/watcha/image/upload/c_fill,h_400,q_80,w_280/v1519955226/vvnr4dqs6rxnxbc4mgvq.jpg'
+    },
+    {
+      movieNm: '레디플레이어 원',
+      rate: 0,
+      posterUrl:
+        'https://dhgywazgeek0d.cloudfront.net/watcha/image/upload/c_fill,h_400,q_80,w_280/v1519955226/vvnr4dqs6rxnxbc4mgvq.jpg'
+    },
+    {
+      movieNm: '레디플레이어 원',
+      rate: 0,
+      posterUrl:
+        'https://dhgywazgeek0d.cloudfront.net/watcha/image/upload/c_fill,h_400,q_80,w_280/v1519955226/vvnr4dqs6rxnxbc4mgvq.jpg'
+    },
+    {
+      movieNm: '레디플레이어 원',
+      rate: 0,
+      posterUrl:
+        'https://dhgywazgeek0d.cloudfront.net/watcha/image/upload/c_fill,h_400,q_80,w_280/v1519955226/vvnr4dqs6rxnxbc4mgvq.jpg'
+    },
+    {
+      movieNm: '레디플레이어 원',
+      rate: 0,
+      posterUrl:
+        'https://dhgywazgeek0d.cloudfront.net/watcha/image/upload/c_fill,h_400,q_80,w_280/v1519955226/vvnr4dqs6rxnxbc4mgvq.jpg'
+    },
+    {
+      movieNm: '레디플레이어 원',
+      rate: 0,
+      posterUrl:
+        'https://dhgywazgeek0d.cloudfront.net/watcha/image/upload/c_fill,h_400,q_80,w_280/v1519955226/vvnr4dqs6rxnxbc4mgvq.jpg'
+    },
+    {
+      movieNm: '레디플레이어 원',
+      rate: 0,
+      posterUrl:
+        'https://dhgywazgeek0d.cloudfront.net/watcha/image/upload/c_fill,h_400,q_80,w_280/v1519955226/vvnr4dqs6rxnxbc4mgvq.jpg'
+    },
+    {
+      movieNm: '레디플레이어 원',
+      rate: 0,
+      posterUrl:
+        'https://dhgywazgeek0d.cloudfront.net/watcha/image/upload/c_fill,h_400,q_80,w_280/v1519955226/vvnr4dqs6rxnxbc4mgvq.jpg'
+    }
+  ];
 
-  constructor() { }
-
-  ngOnInit() {
+  rateFilm(target, index) {
+    if (!this.boxOffice[index].rate) {
+      this.boxOffice[index].rate = target.value;
+    } else if (target.value === this.boxOffice[index].rate) {
+      this.boxOffice[index].rate = 0;
+    } else {
+      this.boxOffice[index].rate = target.value;
+    }
   }
 
+  ngOnInit() {}
 }
