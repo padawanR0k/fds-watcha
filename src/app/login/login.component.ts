5a753dadc8ae3f1d0fb167d566653acea6547a3b
hckang <hckang80@gmail.com>
2018-04-04 13:59:27 +0900

    로그인 유효성 검사와 에러메시지 스타일 작업

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
 
   constructor() { }
 
   ngOnInit() {
+    this.loginForm = new FormGroup({
+      userid: new FormControl('', [
+        Validators.required,
+        Validators.pattern('[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}')
+      ]),
+      password: new FormControl('', [
+        Validators.required
+      ])
+    });
   }
 
+  get userid() {
+    return this.loginForm.get('userid');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
+
+  onSubmit() {
+    console.log(this.loginForm);
+    // this.loginForm.reset();
+  }
 }
