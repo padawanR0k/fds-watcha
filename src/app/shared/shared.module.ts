e7aeb3d9374ce8b07b25bd2fac579d3b89333c60
padawanr0k <padawanr0k@gmail.com>
2018-04-04 15:44:28 +0900

    footer 부분 shared module로 생성

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { BoxOfficeRankingComponent } from './box-office-ranking/box-office-ranking.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [BoxOfficeRankingComponent, HeaderComponent],
+  declarations: [BoxOfficeRankingComponent, HeaderComponent, FooterComponent],
   providers: [],
-  exports: [BoxOfficeRankingComponent, HeaderComponent]
+  exports: [BoxOfficeRankingComponent, HeaderComponent, FooterComponent]
 })
-export class SharedModule { }
+export class SharedModule {}
