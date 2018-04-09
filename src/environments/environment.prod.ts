858df2212ab73defb942b52f454c9963080cc1da
hckang <hckang80@gmail.com>
2018-04-09 15:44:57 +0900

    페이스북 앱아이디 추가

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,7 @@ export const environment = {
   production: true,
   apiUrl: 'https://justdo2t.com/api',
   tokenName: 'access_token',
-  user: 'user'
+  user: 'user',
+  facebookAppId: '556722268030664'
+  // facebookAppId: '432543263851737'
 };
