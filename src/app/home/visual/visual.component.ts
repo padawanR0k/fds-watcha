c07f7d439d61f8a5fd078429fdd42e25a7a0d729
hckang <hckang80@gmail.com>
2018-04-10 12:57:41 +0900

    메인 비주얼 버튼에 액션 추가

--- src/app/home/visual/visual.component.ts
+++ src/app/home/visual/visual.component.ts
@@ -8,105 +8,94 @@ import { Component, OnInit } from '@angular/core';
 export class VisualComponent implements OnInit {
   sliderDirect = 'right';
   currentNum = 0;
+  timer = 4000;
   isAni = false;
   autoPlay = null;
   interval = null;
+  sliderItem;
+  sliderDot;
+  btnArrow;
+  btnDot;
+  OPACITY: number;
 
   constructor() { }
 
   ngOnInit() {
-    let sliderItem = document.querySelectorAll('.main-slider .body li');
-    const arrowBtn = document.querySelector('.main-slider .arrow');
-    const dotBtn = document.querySelector('.main-slider .each');
+    this.sliderItem = document.querySelectorAll('.main-slider .body li');
+    this.sliderItem = [].slice.call(this.sliderItem);
+    this.btnArrow = document.querySelector('.main-slider .arrow');
+    this.btnDot = document.querySelector('.main-slider .each');
 
-    let _sliderItem = [].slice.call(sliderItem);
+    this.autoFn();
+  }
 
-    const mainSlider = () => {
-      let OPACITY = 0;
-      let sliderDot = document.querySelectorAll('.main-slider .each span');
-      let _sliderDot = [].slice.call(sliderDot);
-      _sliderDot.forEach(item => item.classList.remove('current'));
-      _sliderDot[this.currentNum].classList.add('current');
+  frame = () => {
+    if (Math.floor(this.OPACITY) === 1) {
+      clearInterval(this.interval);
+      this.sliderItem.forEach(item => {
+        item.classList.remove('active');
+        item.style.zIndex = null;
+        item.style.opacity = null;
+      });
+      this.sliderItem[this.currentNum].classList.add('active');
+      this.sliderItem[this.currentNum].style.zIndex = '2';
+      this.isAni = false;
+    } else {
+      this.OPACITY += 0.01;
+      this.sliderItem[this.currentNum].style.opacity = this.OPACITY;
+      this.sliderItem[this.currentNum].style.zIndex = '4';
+    }
+  };
 
-      const frame = () => {
-        if (Math.floor(OPACITY) === 1) {
-          clearInterval(this.interval);
-          _sliderItem.forEach(item => {
-            item.classList.remove('active');
-            item.style.zIndex = null;
-            item.style.opacity = null;
-          });
-          _sliderItem[this.currentNum].classList.add('active');
-          _sliderItem[this.currentNum].style.zIndex = '2';
-          this.isAni = false;
-        } else {
-          OPACITY += 0.01;
-          _sliderItem[this.currentNum].style.opacity = OPACITY;
-          _sliderItem[this.currentNum].style.zIndex = '4';
-        }
-      };
-      this.interval = setInterval(frame, 2.5);
-    };
+  mainSlider() {
+    this.OPACITY = 0;
+    this.sliderDot = document.querySelectorAll('.main-slider .each span');
+    this.sliderDot = [].slice.call(this.sliderDot);
+    this.sliderDot.forEach(item => item.classList.remove('current'));
+    this.sliderDot[this.currentNum].classList.add('current');
 
-    const currentMovieNumberInit = state => {
-      if (this.isAni) return;
-      this.isAni = true;
-      if (state === 'right') {
-        if (this.currentNum === _sliderItem.length - 1) {
-          this.currentNum = 0;
-        } else {
-          this.currentNum += 1;
-        }
-      } else if (state === 'left') {
-        if (this.currentNum === 0) {
-          this.currentNum = _sliderItem.length - 1;
-        } else {
-          this.currentNum -= 1;
-        }
+    this.frame();
+    this.interval = setInterval(this.frame, 2.5);
+  };
+
+  currentMovieNumberInit (direct) {
+    if (this.isAni) return;
+    this.isAni = true;
+    if (direct === 'right') {
+      if (this.currentNum === this.sliderItem.length - 1) {
+        this.currentNum = 0;
       } else {
-        this.currentNum = state;
+        this.currentNum += 1;
       }
-      mainSlider();
-    };
-
-    const autoFn = () => {
-      this.autoPlay = setInterval(() => {
-        currentMovieNumberInit('right');
-      }, 4000);
-    };
-
-    const getDots = () => {
-      let html = '';
-      let current = '';
-      for (let i = 0; i < _sliderItem.length; i++) {
-        if (i === 0) {
-          current = ' class="current"';
-        } else {
-          current = '';
-        }
-        html += `<span data-id="${i}"${current}>${i + 1}</span>`;
+    } else if (direct === 'left') {
+      if (this.currentNum === 0) {
+        this.currentNum = this.sliderItem.length - 1;
+      } else {
+        this.currentNum -= 1;
       }
-      dotBtn.innerHTML = html;
-      autoFn();
-    };
+    } else {
+      this.currentNum = direct;
+    }
+    this.mainSlider();
+  };
 
-    getDots();
+  autoFn() {
+    this.autoPlay = setInterval(() => {
+      this.currentMovieNumberInit('right');
+    }, this.timer);
+  };
 
-    // arrowBtn.addEventListener('click', e => {
-    //   if (e.target.nodeName !== 'I') return;
-    //   if (e.target.className === 'icon-left-open-big') {
-    //     this.sliderDirect = 'left';
-    //   } else if (e.target.className === 'icon-right-open-big') {
-    //     this.sliderDirect = 'right';
-    //   }
-    //   currentMovieNumberInit(this.sliderDirect);
-    //   clearInterval(this.autoPlay);
-    // });
+  nextMovie(direct: string) {
+    this.sliderDirect = direct;
+    
+    this.currentMovieNumberInit(this.sliderDirect);
+    clearInterval(this.autoPlay);
+    this.autoFn();
+  }
 
-    // dotBtn.addEventListener('click', e => {
-    //   if (this.currentNum === +e.target.dataset.id || e.target.nodeName !== 'SPAN') return;
-    //   currentMovieNumberInit(+e.target.dataset.id);
-    //   clearInterval(this.autoPlay);
-    // });
+  selectMovie(target: number) {
+    this.currentMovieNumberInit(target);
+    clearInterval(this.autoPlay);
+    this.autoFn();
   }
 }
