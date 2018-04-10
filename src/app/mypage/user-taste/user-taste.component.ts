c5566f053c1e019d1cc139f79ae69bd4c7a23593
padawanr0k <padawanr0k@gmail.com>
2018-04-10 12:01:16 +0900

    마이페이지 라우팅 추가

---
+++ src/app/mypage/user-taste/user-taste.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-taste',
+  templateUrl: './user-taste.component.html',
+  styleUrls: ['./user-taste.component.scss']
+})
+export class UserTasteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/mypage/watched-movies/watched-movies.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-watched-movies',
+  templateUrl: './watched-movies.component.html',
+  styleUrls: ['./watched-movies.component.scss']
+})
+export class WatchedMoviesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
