de1238001436c02d14136a9f15bfc6688694e6e2
jirolrol <jirolrol@gmail.com>
2018-04-18 16:49:17 +0900

    영화상세페이지 감독모달 스타일 추가작업, 마이페이지 API 수정

--- src/app/shared/movie-poster.interface.ts
+++ src/app/shared/movie-poster.interface.ts
@@ -1,32 +1,31 @@
 export interface MoviePoster {
-    id: number;
-    title_ko: string;
-    title_en: string;
-    rating_avg: number;
-    nation: string;
-    poster_image: string;
-    genre: [
-        {
-            id: number;
-            genre: string;
-        },
-        {
-            id: number;
-            genre: string;
-        },
-        {
-            id: number;
-            genre: string;
-        },
-        {
-            id: number;
-            genre: string;
-        },
-        {
-            id: number;
-            genre: string;
-        }
-    ];
-    running_time: number;
+    count: number;
+    next: string;
+    previous: string;
+    results: [
+                {
+                    id: number;
+                    title_ko: string;
+                    title_en: string;
+                    rating_avg: number;
+                    nation: string;
+                    poster_image: string;
+                    genre: [
+                        {
+                            id: number;
+                            genre: string;
+                        }
+                    ],
+                    running_time: number;
+                    login_user_checked: {
+                        id: number;
+                        user_want_movie: boolean;
+                        user_watched_movie: boolean;
+                        rating: number;
+                        comment: string;
+                        user: number;
+                        movie: number;
+                    }
+                }
+            ];
     }
-
--- src/app/shared/movie-poster/movie-poster.component.ts
+++ src/app/shared/movie-poster/movie-poster.component.ts
@@ -14,19 +14,24 @@ import { CommnetDialogService } from '../../core/comment-dialog.service';
 export class MoviePosterComponent implements OnInit {
   @Input() moviePoster: MoviePoster;
   rateScore = [0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5];
-  constructor(public http: HttpClient, public movieDetailService: MovieDetailService, public commnetDialogService: CommnetDialogService) {
-  }
+  constructor(
+    public http: HttpClient,
+    public movieDetailService: MovieDetailService,
+    public commnetDialogService: CommnetDialogService
+  ) { }
+
   rateFilm(target) {
-    if (!this.moviePoster.rate || target.value !== this.moviePoster.rate  ) {
-      this.moviePoster.rate = target.value;
+    if (!this.moviePoster.results[0].login_user_checked.rating || target.value !==
+      this.moviePoster.results[0].login_user_checked.rating  ) {
+      this.moviePoster.results[0].login_user_checked.rating = target.value;
 
       // 평가가 된적이 없다는 것이므로 평가한 별점을 db에 보낸다.
       // 없엇던 리소스를 추가하는 것이기 때문에 put을 통해 요청한다.
-    } else if (target.value === this.moviePoster.rate) {
+    } else if (target.value === this.moviePoster.results[0].login_user_checked.rating) {
 
       // 평가한 별점이 이미 평가한 별점과 같다는 것으로 평가를 취소한다. === db의 평가를 false로 만든다.
       // 있던 rate를 수정하는 것이기 때문에 patch를 통해 요청한다.
-      this.moviePoster.rate = 0;
+      this.moviePoster.results[0].login_user_checked.rating = 0;
     }
   }
   ngOnInit() {}
