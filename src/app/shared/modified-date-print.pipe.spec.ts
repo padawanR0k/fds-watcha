ea425011ba2228534665a2ed72b39c2ab4fa5bd1
hckang <hckang80@gmail.com>
2018-04-23 14:43:39 +0900

    영화상세모달 코멘트 작성일 출력

---
+++ src/app/shared/modified-date-print.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { ModifiedDatePrintPipe } from './modified-date-print.pipe';
+
+describe('ModifiedDatePrintPipe', () => {
+  it('create an instance', () => {
+    const pipe = new ModifiedDatePrintPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
