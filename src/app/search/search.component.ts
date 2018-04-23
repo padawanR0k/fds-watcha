d373e0ebf5928965ad07287b6ce1894d7ad3cef5
jirolrol <jirolrol@gmail.com>
2018-04-23 18:16:36 +0900

    search 페이지, 영화 상세 페이지 감독/주연 API 연동

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,8 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+import { AuthService } from '../core/auth/services/auth.service';
 import { PreloaderService } from '../shared/preloader';
-// import { MoviePoster } from '../shared/movie-poster.interface';
+
+import { MoviePoster } from '../shared/movie-poster.interface';
+
+import { environment } from '../../environments/environment';
+
 
 @Component({
   selector: 'app-search',
@@ -11,117 +16,30 @@ import { PreloaderService } from '../shared/preloader';
 })
 export class SearchComponent implements OnInit {
 
-  moviePosters: any;
+  moviePosters: object;
+  count: number;
   searchString = '인셉션';
+  appUrl = environment.apiUrl;
 
-  url = 'http://localhost:3000/movieposter';
   constructor(
     public http: HttpClient,
-    public preloader: PreloaderService) { }
-
+    private auth: AuthService,
+    public preloader: PreloaderService
+  ) { }
 
   ngOnInit() {
     this.preloader.show();
-    this.http.get('http://localhost:3000/movieposter')
+    this.auth.getToken();
+    this.http.get<MoviePoster>(`${this.appUrl}/movie/search/?movie=바`,
+      { headers: { Authorization: `Token ${this.auth.getToken()}` } })
       .subscribe(res => {
         setTimeout(() => {
-          this.moviePosters = res;
+          this.moviePosters = res.results;
+          this.count = res.count;
           this.preloader.hide();
         }, 2000);
       });
   }
 
-  // moviePosters: MoviePoster[];
-  // searchString = '인셉션';
 
-  // constructor() {
-  //   this.moviePosters = [
-  //     {
-  //       movieNm: '지금 만나러 갑니다',
-  //       rate: 0,
-  //       posterUrl: '../../assets/images/bewithyou.jpg',
-  //       year: 2017,
-  //       title: '지금 만나러 갑니다',
-  //       director: '이장훈',
-  //       actor: '소지섭, 손예진'
-  //      },
-  //     {
-  //       movieNm: '레디플레이어 원',
-  //       rate: 0,
-  //       posterUrl: '../../assets/images/readyplayone.jpg',
-  //       year: 2018,
-  //       title: 'Ready Player One',
-  //       director: '스티븐 스필버그',
-  //       actor: '타이 셰리던, 올리비아 쿡, 벤 멘델슨'
-  //     },
-  //     {
-  //       movieNm: '바람 바람 바람',
-  //       rate: 0,
-  //       posterUrl: '../../assets/images/windwindwind.jpg',
-  //       year: 2017,
-  //       title: 'What a Man Wants',
-  //       director: '이병헌',
-  //       actor: '위하준, 박지현, 오아연, 문예원, 박성훈, 유제윤, 박지현, 오아연, 문예원, 박성훈, 유제윤, 박지현, 오아연, 문예원, 박성훈, 유제윤'
-  //      },
-  //     {
-  //       movieNm: '곤지암',
-  //       rate: 0,
-  //       posterUrl: '../../assets/images/gonjiam.jpg',
-  //       year: 2018,
-  //       title: '곤지암',
-  //       director: '정범식',
-  //       actor: '위하준, 박지현, 오아연, 문예원, 박성훈, 유제윤'
-  //      },
-  //     {
-  //       movieNm: '지금 만나러 갑니다',
-  //       rate: 0,
-  //       posterUrl: '../../assets/images/bewithyou.jpg',
-  //       year: 2017,
-  //       title: '지금 만나러 갑니다',
-  //       director: '이장훈',
-  //       actor: '소지섭, 손예진'
-  //      },
-  //     {
-  //       movieNm: '레디플레이어 원',
-  //       rate: 0,
-  //       posterUrl: '../../assets/images/readyplayone.jpg',
-  //       year: 2018,
-  //       title: 'Ready Player One',
-  //       director: '스티븐 스필버그',
-  //       actor: '타이 셰리던, 올리비아 쿡, 벤 멘델슨'
-  //     },
-  //     {
-  //       movieNm: '바람 바람 바람',
-  //       rate: 0,
-  //       posterUrl: '../../assets/images/windwindwind.jpg',
-  //       year: 2017,
-  //       title: 'What a Man Wants',
-  //       director: '이병헌',
-  //       actor: '위하준, 박지현, 오아연, 문예원, 박성훈, 유제윤, 박지현, 오아연, 문예원, 박성훈, 유제윤, 박지현, 오아연, 문예원, 박성훈, 유제윤'
-  //      },
-  //     {
-  //       movieNm: '곤지암',
-  //       rate: 0,
-  //       posterUrl: '../../assets/images/gonjiam.jpg',
-  //       year: 2018,
-  //       title: '곤지암',
-  //       director: '정범식',
-  //       actor: '위하준, 박지현, 오아연, 문예원, 박성훈, 유제윤'
-  //      }
-  //   ];
-  // }
-
-  // rateScore = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  // rateFilm(target, index) {
-  //   if (!this.moviePosters[index].rate) {
-  //     this.moviePosters[index].rate = target.value;
-  //   } else if (target.value === this.moviePosters[index].rate) {
-  //     this.moviePosters[index].rate = 0;
-  //   } else {
-  //     this.moviePosters[index].rate = target.value;
-  //   }
-  // }
-
-  // ngOnInit() {
-  // }
 }
