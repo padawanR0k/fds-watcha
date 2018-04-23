ed46b6e9b1a60a2565b3f10d3f0e9591e4f3e74e
hckang <hckang80@gmail.com>
2018-04-23 14:45:28 +0900

    영화상세모달 D-day 출력

---
+++ src/app/shared/d-day-print.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { DDayPrintPipe } from './d-day-print.pipe';
+
+describe('DDayPrintPipe', () => {
+  it('create an instance', () => {
+    const pipe = new DDayPrintPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
