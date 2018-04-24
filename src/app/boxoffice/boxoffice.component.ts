241d6a73e2e43ea72b03d315c0ab4c0a8767e175
hckang <hckang80@gmail.com>
2018-04-24 16:20:16 +0900

    데이터 바인딩 및 스타일 작업

--- src/app/boxoffice/boxoffice.component.ts
+++ src/app/boxoffice/boxoffice.component.ts
@@ -3,13 +3,15 @@ import { Component, OnInit } from '@angular/core';
 import { BoxOfficePagenationService } from '../home/box-office-pagenation.service';
 import { MovieDetailService } from '../core/movie-detail.service';
 
+import { BoxofficeMovie } from '../shared/boxoffice-movie.interface';
+
 @Component({
   selector: 'app-boxoffice',
   templateUrl: './boxoffice.component.html',
   styleUrls: ['./boxoffice.component.scss']
 })
 export class BoxofficeComponent implements OnInit {
-  movieDetail;
+  movieDetail: BoxofficeMovie;
 
   constructor(
     public boxOfficePage: BoxOfficePagenationService, 
@@ -23,7 +25,6 @@ export class BoxofficeComponent implements OnInit {
       .subscribe(
         movie => {
           this.movieDetail = movie;
-          console.log('[Movie1]', this.movieDetail);
         },
         ({ error }) => {
           console.log('ERROR', error.message);
