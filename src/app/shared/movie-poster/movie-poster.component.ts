80f46a8ad76086f7742297f091fb0bacb8be1fb3
padawanr0k <padawanr0k@gmail.com>
2018-04-24 14:08:18 +0900

    별점 평가 기능 및 이미 평가한 별점 불러오기기능 완성

--- src/app/shared/movie-poster/movie-poster.component.ts
+++ src/app/shared/movie-poster/movie-poster.component.ts
@@ -8,7 +8,6 @@ import { CommnetDialogService } from '../../core/comment-dialog.service';
 import { environment } from '../../../environments/environment.prod';
 import { AuthService } from '../../core/auth/services/auth.service';
 import { UserService } from '../../core/auth/services/user.service';
-import { UserCheckedService, UserWatched } from '../../core/user-checked.service';
 
 export interface CheckMovie {
   rating: number;
@@ -70,24 +69,27 @@ export class MoviePosterComponent implements OnInit{
   userActionResponse: UserActionResponse;
   apiUrl = environment.apiUrl;
   httpOptions: HttpHeaders;
+
   rating: number;
   checkedId: number;
+  comment: boolean;
+  want: boolean;
+  watched: boolean;
+  userToMovieId: number;
 
   constructor(
     public http: HttpClient,
-    public userChecked: UserCheckedService,
     public movieDetailService: MovieDetailService,
     public commnetDialogService: CommnetDialogService,
     private authService: AuthService,
     private user: UserService,
   ) {
+
   }
 
 
-  rateFilm(target, rate, moviePK) {
-    // console.log(target.value);
+  rateFilm(target, rate, moviePK, InputUserToMovieID ) {
     console.log(arguments);
-    console.log(typeof this.rating, typeof rate)
     const headers = (this.httpOptions = new HttpHeaders()
       .set('Authorization', `token ${this.authService.getToken()}`)
       .set('Content-Type', 'application/json'));
@@ -96,38 +98,42 @@ export class MoviePosterComponent implements OnInit{
     this.userAction = {
       movie: moviePK,
       rating: rate,
+      user_want_movie: false,
     };
-
-
-    if ( !this.rating ) {
+    console.log(`userToMovieId ${InputUserToMovieID}`);
+    if (this.userToMovieId === 0) {
       // 평가내역이 없다면 내역을 생성한다.
       this.userAction.user_watched_movie = true;
-      this.userAction.user_want_movie = false;
       console.log(this.userAction, '새로운 객체 생성');
+
       this.http.post<UserActionResponse>(`${this.apiUrl}/movie/user-checked-movie/create/`, this.userAction, { headers })
         .subscribe(res => {
-          this.userActionResponse = res;
           this.rating = +res.rating;
+          this.want = res.user_want_movie;
+          this.watched = res.user_watched_movie;
+          this.userToMovieId = res.id;
         });
     } else if (+this.rating === rate) {
       // 평가한 점수 === 클릭한점수면 별을 삭제한다.
-      this.userAction.comment = '';
-      this.userAction.rating = 0;
+      this.userAction.rating = null;
       this.userAction.user_watched_movie = false;
-      this.userAction.id = this.checkedId;
+      this.userAction.id = this.userToMovieId;
       this.user.getUsers().subscribe( res => this.userAction.user = res.pk);
       console.log(this.userAction, '삭제실행');
+
       this.http.put<UserActionResponse>(`${this.apiUrl}/movie/user-checked-movie/${this.userAction.id}/`,
         this.userAction, { headers })
         .subscribe(res => {
           this.userActionResponse = res;
           this.rating = null;
         });
-    } else if ( this.rating && +this.rating !== rate ) {
+    } else if ( this.userToMovieId !== 0 && +this.rating !== rate ) {
       // 평가한 별점이 존재하는데, 지금 평가한 별점과 다르면 수정한다.
-      this.userAction.id = this.checkedId;
+      this.userAction.id = this.userToMovieId;
+      this.userAction.user_watched_movie = true;
       this.user.getUsers().subscribe( res => this.userAction.user = res.pk);
       console.log(this.userAction, '수정 실행');
+
       this.http.put<UserActionResponse>(`${this.apiUrl}/movie/user-checked-movie/${this.userAction.id}/`, this.userAction, { headers })
         .subscribe(res => {
           this.userActionResponse = res;
@@ -135,17 +141,67 @@ export class MoviePosterComponent implements OnInit{
         });
     }
   }
-  isWatched() {
-    this.userChecked.userWatched.map(item => {
-      if (item.id === this.moviePoster.id) {
-        this.rating = item.login_user_checked.rating;
-        this.checkedId = item.login_user_checked.id;
+  // isWatched() {
+  //   this.userChecked.userWatched.map(item => {
+  //     if (item.id === this.moviePoster.id) {
+  //       this.rating = item.login_user_checked.rating;
+  //       this.checkedId = item.login_user_checked.id;
+  //     }
+  //   });
+  // }
+  WantMovie(moviePK, InputUserToMovieID, want) {
+    console.log('보고싶어요 클릭됨');
+    // http클라이언트로 생성 혹은 수정한다.
+    const headers = (this.httpOptions = new HttpHeaders()
+      .set('Authorization', `token ${this.authService.getToken()}`)
+      .set('Content-Type', 'application/json'));
+    this.userAction = {
+        movie: moviePK,
+        rating: null,
+        user_watched_movie: false,
+    };
+    if ( want !== true) {
+      if ( Object.keys(this.moviePoster.login_user_checked).length === 1  ) {
+        // 생성 api
+        this.userAction.user_want_movie = true;
+        this.http.put<UserActionResponse>(`${this.apiUrl}/movie/user-checked-movie/create/`, this.userAction, { headers })
+        .subscribe(res => {
+          this.want = res.user_want_movie;
+          this.rating  = null;
+          this.userToMovieId = res.id;
+        });
+      } else {
+        // 수정 api
+        this.userAction.user_want_movie = true;
+        this.userAction.id = InputUserToMovieID;
+
+        this.user.getUsers().subscribe(res => (this.userAction.user = res.pk));
+        this.http.put<UserActionResponse>(`${this.apiUrl}/movie/user-checked-movie/${this.userAction.id}/`, this.userAction, { headers })
+        .subscribe(res => {
+          this.want = res.user_want_movie;
+          this.rating  = null;
+        });
       }
-    });
+    } else {
+      // 수정 api로 삭제
+      this.userAction.user_want_movie = false;
+      this.userAction.id = InputUserToMovieID;
+
+      this.user.getUsers().subscribe(res => (this.userAction.user = res.pk));
+      this.http.put<UserActionResponse>(`${this.apiUrl}/movie/user-checked-movie/${this.userAction.id}/`, this.userAction, { headers })
+      .subscribe(res => {
+        this.userActionResponse = res;
+      });
+    }
   }
   ngOnInit() {
     // console.log(this.userChecked.userWatched, '내가 봤거나 보고싶어요 누른 영화들의 정보');
     // console.log(this.moviePoster , '그려질 영화의 정보');
-    this.isWatched();
+    const login_user = this.moviePoster.login_user_checked;
+    console.log(this.moviePoster);
+    this.rating = Object.keys(login_user).length === 1 ? 0 : +login_user.rating;
+    this.want = Object.keys(login_user).length === 1 ? false : login_user.user_want_movie;
+    this.comment = login_user.comment === '' ? false : login_user.user_watched_movie;
+    this.userToMovieId = Object.keys(login_user).length === 1 ? 0 : login_user.id;
   }
 }
