ed36ffe0028a3aa099e603a045affc071c24499c
jirolrol <jirolrol@gmail.com>
2018-04-25 16:56:02 +0900

    박스오피스 프리로더 추가

--- src/app/boxoffice/boxoffice.component.ts
+++ src/app/boxoffice/boxoffice.component.ts
@@ -4,6 +4,7 @@ import { BoxOfficePagenationService } from '../home/box-office-pagenation.servic
 import { MovieDetailService } from '../core/movie-detail.service';
 
 import { BoxofficeMovie } from '../shared/boxoffice-movie.interface';
+import { PreloaderService } from '../shared/preloader';
 
 @Component({
   selector: 'app-boxoffice',
@@ -12,19 +13,22 @@ import { BoxofficeMovie } from '../shared/boxoffice-movie.interface';
 })
 export class BoxofficeComponent implements OnInit {
   movieDetail: BoxofficeMovie;
-
+  mainView = false;
   constructor(
     public boxOfficePage: BoxOfficePagenationService, 
-    public movieDetailService: MovieDetailService
+    public movieDetailService: MovieDetailService,
+    public preloader: PreloaderService
   ) { }
-  
+
   ngOnInit() {
+    this.preloader.show();
     this.boxOfficePage.loadTopFive();
-    
     this.movieDetailService.getBoxOfficeDetail()
       .subscribe(
         movie => {
           this.movieDetail = movie;
+          this.preloader.hide();
+          this.mainView = true;
         },
         ({ error }) => {
           console.log('ERROR', error.message);
